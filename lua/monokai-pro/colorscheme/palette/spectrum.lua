4b1cf960686b194d4b1bb320d7de0689c663b042
Jack <jr@jrh.net>
2024-08-29 23:04:07 -0700

    Update spectrum.lua

--- lua/monokai-pro/colorscheme/palette/spectrum.lua
+++ lua/monokai-pro/colorscheme/palette/spectrum.lua
@@ -1,18 +1,18 @@
 ---@type Palette
 return {
-  dark2 = "#131313",
-  dark1 = "#191919",
-  background = "#222222",
-  text = "#f7f1ff",
-  accent1 = "#fc618d",
-  accent2 = "#fd9353",
-  accent3 = "#fce566",
-  accent4 = "#7bd88f",
-  accent5 = "#5ad4e6",
-  accent6 = "#948ae3",
-  dimmed1 = "#bab6c0",
-  dimmed2 = "#8b888f",
-  dimmed3 = "#69676c",
-  dimmed4 = "#525053",
-  dimmed5 = "#363537",
+  dark2 = "#19181a",       -- Mapped from base0
+  dark1 = "#221f22",       -- Mapped from bg-alt
+  background = "#2d2a2e",  -- Mapped from bg
+  text = "#fcfcfa",        -- Mapped from fg
+  accent1 = "#ff6188",     -- Mapped from magenta
+  accent2 = "#fc9867",     -- Mapped from orange
+  accent3 = "#ffd866",     -- Mapped from yellow
+  accent4 = "#a9dc76",     -- Mapped from green
+  accent5 = "#78dce8",     -- Mapped from blue
+  accent6 = "#ab9df2",     -- Mapped from violet
+  dimmed1 = "#c1c0c0",     -- Mapped from base7
+  dimmed2 = "#727072",     -- Mapped from grey/base6
+  dimmed3 = "#4c4a4d",     -- Mapped from base5
+  dimmed4 = "#383539",     -- Mapped from base4
+  dimmed5 = "#2b2b2b",     -- Mapped from base3
 }
